ee6b50dcf35051e8b12956018312429f916ac3e3
wai-lau <wl.wailau@gmail.com>
2026-04-16 18:43:56 -0400

    compose

--- api/main.py
+++ api/main.py
@@ -1,5 +1,14 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Security, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.staticfiles import StaticFiles
+
+API_KEY = os.environ["API_KEY"]
+bearer = HTTPBearer()
+
+def require_auth(credentials: HTTPAuthorizationCredentials = Security(bearer)):
+    if credentials.credentials != API_KEY:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API key")
 
 app = FastAPI()
 
